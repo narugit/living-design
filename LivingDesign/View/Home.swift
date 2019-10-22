ba2ad783fda540c52421027f48b289e33a21a2d0
narugit <narusens@gmail.com>
2019-10-22 18:27:23 +0900

    Add home screen layout

--- LivingDesign/View/Home.swift
+++ LivingDesign/View/Home.swift
@@ -9,57 +9,64 @@
 import Foundation
 import UIKit
 
-class Home3: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
-    //"dog1""dog2""dog3""dog4""dog5"は画像の名前を記入。
-    let photos = ["washing-machine" ]
+class Home: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    private let photos = ["washing-machine", "copier", "oven", "tv" ]
+    private let names = ["洗濯機", "コピー機", "オーブン", "テレビ"]
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.titleLabel.text = "家電"
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        self.collectionView.collectionViewLayout = layout
     }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
-        print("fuga")
-        // "Cell" はストーリーボードで設定したセルのID
-        let testCell:UICollectionViewCell =
-            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
-                                               for: indexPath)
-        
-        // Tag番号を使ってImageViewのインスタンス生成
-        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
-        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
-        let cellImage = UIImage(named: "washing-machine")
-        // UIImageをUIImageViewのimageとして設定
-        imageView.image = cellImage
-        
-        // Tag番号を使ってLabelのインスタンス生成
-        let label = testCell.contentView.viewWithTag(2) as! UILabel
-        label.text = photos[indexPath.row]
-        
-        return testCell
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // section数は１つ  
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        numberOfItemsInSection section: Int) -> Int {
-        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
-        return photos.count;
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     // 画面生成後の処理
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("hoge")
     }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let cell: UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                               for: indexPath)
+
+        let imageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let cellImage = UIImage(named: photos[indexPath.row])
+        imageView.image = cellImage
+        // imageViewのサイズ確認用
+        // imageView.backgroundColor = .blue
+
+        let label = cell.contentView.viewWithTag(2) as! UILabel
+        label.text = names[indexPath.row]
+        // セルのサイズ確認用
+        // cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize : CGFloat = self.view.bounds.width / 5
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        // 要素数を格納
+        return self.photos.count;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
