12d882ce499c849c06791cb7328c2a22e2bdfd94
narugit <narusens@gmail.com>
2019-11-12 12:58:23 +0900

    add setting screen

---
+++ LivingDesign/View/Setting.swift
@@ -0,0 +1,15 @@
+//
+//  Setting.swift
+//  LivingDesign
+//
+//  Created by naru on 2019/11/12.
+//  Copyright © 2019 DSN. All rights reserved.
+//
+
+import UIKit
+
+class Setting: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
