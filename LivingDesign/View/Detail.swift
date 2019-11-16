9264cfe5534779bc3ee17faeebfd45f93acaecd7
29maron <maron.panic@gmail.com>
2019-11-17 05:36:19 +0900

    detailとeditの編集
    quit

--- LivingDesign/View/Detail.swift
+++ LivingDesign/View/Detail.swift
@@ -6,9 +6,8 @@
 //  Copyright © 2019 DSN. All rights reserved.
 //
 
-import UIKit
 import Foundation
-import PDFKit
+import UIKit
 import RealmSwift
 
 class Detail: UIViewController {
@@ -17,53 +16,66 @@ class Detail: UIViewController {
 
     //@IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var category: UILabel!
-    @IBOutlet weak var model: UILabel!
+    @IBOutlet weak var genre: UILabel!
+    @IBOutlet weak var modelNumber: UILabel!
     @IBOutlet weak var price: UILabel!
-    @IBOutlet weak var date: UILabel!
-    @IBOutlet weak var buyreason: UILabel!
-    @IBOutlet weak var assessment: UILabel!
-    @IBOutlet weak var candidate: UILabel!
-    @IBOutlet weak var memo: UILabel!
- 
-    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var purchaseDate: UILabel!
+    @IBOutlet weak var warrantyPeriod: UILabel!
+    @IBOutlet weak var reason: UILabel!
+    @IBOutlet weak var comfort: UILabel!
+    @IBOutlet weak var otherTargets: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
+    
+    var currentSelected : Int?
+    private let wireframe: RootViewWireframe = RootViewWireframe()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let realm = try! Realm()
-        let shareData = realm.objects(ShareInNavigation.self)
-        print(shareData)
-        
-        
-        
-        name.text = "洗濯機"
-        
-        
-        
-        productImage.image = UIImage(named: "washing-machine")
-        
-        
-        
+        let shareData = realm.objects(ShareInNavigation.self).first
+        print(shareData!.data)
+        let inputId:String = shareData!.data
+        let thisItem = realm.objects(Item.self).filter("id == %@",inputId).first
+        print(thisItem)
+        self.name.text = thisItem!.name
+        self.genre.text = thisItem!.genre
+        self.modelNumber.text = thisItem!.modelNumber
+        //self.price.text = thisItem!.price
+        self.purchaseDate.text = thisItem!.purchaseDate
+        self.reason.text = thisItem!.reason
+        //self.comfort.text = thisItem!
+        self.warrantyPeriod.text = thisItem!.warrantyPeriod
+        self.otherTargets.text = thisItem!.otherTargets
+        //self.photo.image = thisItem!.photo
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func edit(_ sender: Any) {
+
+    @IBAction func TouchUpInsideDepositButton(_ sender: Any) {
     }
     
     
-    @IBAction func change(_ sender: Any) {
+    @IBAction func TouchUpInsideEditButton(_ sender: Any) {
+        let nextStoryBoard = UIStoryboard(name: "Edit", bundle: nil)
+            let nextViewController = nextStoryBoard.instantiateViewController(withIdentifier: "EditViewControllerID")
+            self.wireframe.transition(to: nextViewController)
     }
     
-    
-    @IBAction func discard(_ sender: Any) {
+    @IBAction func TouchUpInsideChangeButton(_ sender: Any) {
+        let nextStoryBoard = UIStoryboard(name: "NewProduct", bundle: nil)
+            let nextViewController = nextStoryBoard.instantiateViewController(withIdentifier: "NewProductViewControllerID")
+            self.wireframe.transition(to: nextViewController)
     }
-    
-    
+
+
 }
 
 
--- LivingDesign/View/NewProduct.swift
+++ LivingDesign/View/NewProduct.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2019 DSN. All rights reserved.
 //
 
-import UIKit
 import Foundation
-import PDFKit
+import UIKit
+import RealmSwift
 
 class NewProduct: UIViewController {
     
@@ -22,41 +22,22 @@ class NewProduct: UIViewController {
     @IBOutlet weak var assessment: UILabel!
     @IBOutlet weak var candidate: UILabel!
     @IBOutlet weak var memo: UILabel!
-    @IBOutlet weak var productImage: PDFView!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    var currentSelected : Int?
+    private let wireframe: RootViewWireframe = RootViewWireframe()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        if let pdfURL = Bundle.main.url(forResource: "washing-machine", withExtension: "pdf") {
-            if let pdf = PDFDocument(url: pdfURL) {
-                productImage.document = pdf
-                // PDFの拡大率を調整する
-                productImage.autoScales = true
-                // 表示モード
-                productImage.displayMode = .singlePageContinuous
-            }
-        }
-        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func TouchUpInsideReturnButton(_ sender: Any) {
     }
     
-    @IBAction func edit(_ sender: Any) {
+    @IBAction func TouchUpInsideRegisterButton(_ sender: Any) {
     }
     
     
-    @IBAction func change(_ sender: Any) {
-    }
-    
-    
-    @IBAction func discard(_ sender: Any) {
-    }
-    
-    
-}
 
+}
